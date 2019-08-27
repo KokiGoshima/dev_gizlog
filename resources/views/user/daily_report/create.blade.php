8eb5b6774b94a37f70c4d3038d3962e7caac179f
Koki Goshima <gosh.namnam0705@gmail.com>
2019-08-27 15:17:13 +0900

    Formファサード導入のためにconfig/appに追加

--- resources/views/user/daily_report/create.blade.php
+++ resources/views/user/daily_report/create.blade.php
@@ -4,10 +4,10 @@
 <h2 class="brand-header">日報作成</h2>
 <div class="main-wrap">
   <div class="container">
-      <input class="form-control" name="user_id" type="hidden">
-      <div class="form-group form-size-small">
-    <input class="form-control" name="reporting_time" type="date">
-    <span class="help-block"></span>
+    <input class="form-control" name="user_id" type="hidden">
+    <div class="form-group form-size-small">
+      <input class="form-control" name="reporting_time" type="date">
+      <span class="help-block"></span>
     </div>
     <div class="form-group">
       <input class="form-control" placeholder="Title" name="title" type="text">
