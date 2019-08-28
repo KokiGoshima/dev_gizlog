65144343bfdb3c4c9fc762ec639c527e8306a310
Koki Goshima <gosh.namnam0705@gmail.com>
2019-08-28 23:14:44 +0900

    細かい修正

--- resources/views/user/daily_report/index.blade.php
+++ resources/views/user/daily_report/index.blade.php
@@ -23,7 +23,7 @@
       <tbody>
         @foreach ($reports as $report)
           <tr class="row">
-            <td class="col-xs-2">{{ $report->reporting_time->format('m/d()D') }}</td>
+            <td class="col-xs-2">{{ $report->reporting_time->format('m/d(D)') }}</td>
             <td class="col-xs-3">{{ $report->title }}</td>
             <td class="col-xs-5">{{ mb_strimwidth($report->content, 0, 50, '...') }}</td>
             <td class="col-xs-2"><a class="btn" href="{{ route('dailyReport.show', $report->id) }}"><i class="fa fa-book"></i></a></td>
