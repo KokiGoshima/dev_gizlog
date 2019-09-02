91da7a02599dc1ff881cb33a3baba879173ceb4b
Koki Goshima <gosh.namnam0705@gmail.com>
2019-09-02 17:29:25 +0900

    questionsテーブル作成

--- database/migrations/2019_09_02_080202_create_questions_table.php
+++ database/migrations/2019_09_02_080202_create_questions_table.php
@@ -15,7 +15,12 @@ class CreateQuestionsTable extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('tag_category_id');
+            $table->string('title');
+            $table->text('content');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
