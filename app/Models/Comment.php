4adea6feefacf1e342fa104eea38d33c98fdb960
Koki Goshima <gosh.namnam0705@gmail.com>
2019-09-03 00:05:28 +0900

    commentsテーブル fillable設定

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+    protected $fillable = [
+        'user_id',
+        'question_id',
+        'content',
+    ]
+
     public function question()
     {
         $this->belongsTo('App\Models\Question');
