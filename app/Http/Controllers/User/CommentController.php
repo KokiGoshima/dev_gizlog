f988989c95308430a6ac384e0cdc0f06417ae004
Koki Goshima <gosh.namnam0705@gmail.com>
2019-09-03 17:58:21 +0900

    no message

--- app/Http/Controllers/User/CommentController.php
+++ app/Http/Controllers/User/CommentController.php
@@ -33,9 +33,9 @@ class CommentController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CommentRequest $request)
     {
-        //
+
     }
 
     /**
