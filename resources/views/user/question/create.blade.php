c16fc30e165ba69388b15d492a63a58af2ac4e6f
Koki Goshima <gosh.namnam0705@gmail.com>
2019-09-03 11:32:10 +0900

    improve: formタグをFormファサードで書き換える。

--- resources/views/user/question/create.blade.php
+++ resources/views/user/question/create.blade.php
@@ -4,7 +4,7 @@
 <h2 class="brand-header">質問投稿</h2>
 <div class="main-wrap">
   <div class="container">
-    <form>
+    {!! Form::open(['route' => 'question.store']) !!}
       <div class="form-group">
         <select name='tag_category_id' class = "form-control selectpicker form-size-small" id="pref_id">
           <option value="">Select category</option>
@@ -24,7 +24,7 @@
         <span class="help-block"></span>
       </div>
       {!! Form::submit('create', ['class' => 'btn btn-success pull-right', 'name' => 'confirm']) !!}
-    </form>
+    {!! Form::close() !!}
   </div>
 </div>
 
