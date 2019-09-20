4b7c5f5a4e6e3c41a5d434b4cde326e1b117ed6a
Koki Goshima <gosh.namnam0705@gmail.com>
2019-09-20 11:10:15 +0900

    add: Attendancesテーブル作成

---
+++ database/migrations/2019_09_20_010752_create_attendances_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->date('date');
+            $table->time('start_time')->nullable();
+            $table->time('end_time')->nullable();
+            $table->text('absence_reason')->nullable();
+            $table->text('correction_reason')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+}
