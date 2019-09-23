a890f3b3ee6ba175a49ee1ed8b5a5eff04aa85dc
Koki Goshima <gosh.namnam0705@gmail.com>
2019-09-23 15:23:45 +0900

    fix: AttendansesTableSeederを変更

--- database/seeds/AttendancesTableSeeder.php
+++ database/seeds/AttendancesTableSeeder.php
@@ -76,7 +76,7 @@ class AttendancesTableSeeder extends Seeder
             [
                 'user_id' => 6,
                 'date' => '2019-07-03',
-                'start_time' => '2019-07-03 09:54:15',
+                'start_time' => null,
                 'end_time' => null,
                 'absence_reason' => '熱が出て15:00に早退しました',
                 'correction_reason' => null,
@@ -84,13 +84,13 @@ class AttendancesTableSeeder extends Seeder
                 'correction_presence' => 0,
             ],
             [
-                'user_id' => 7,
+                'user_id' => 8,
                 'date' => '2019-07-03',
-                'start_time' => '2019-07-03 09:30:15',
+                'start_time' => '2019-07-03 08:50:15',
                 'end_time' => '2019-07-03 18:54:15',
-                'absence_reason' => 'dcdfkvndfm',
-                'correction_reason' => 'vldfvsk',
-                'absence_presence' => 1,
+                'absence_reason' => null,
+                'correction_reason' => '退勤押し忘れました。',
+                'absence_presence' => 0,
                 'correction_presence' => 1,
             ],
         ]);
